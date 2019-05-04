70f41bb4885de77ff1a2edc1c906f40cfbc195e4
Rebecca <rrossman25@gmail.com>
2019-05-04 19:17:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ function total() {
 
 function removeFromCart(item) {
   for (let i = 0; i < cart.length; i++){
-     if (item === cart[i][itemName]){
+     if (item === cart[i]['itemName']){
         cart.splice(i,1);
         return cart;
      }
